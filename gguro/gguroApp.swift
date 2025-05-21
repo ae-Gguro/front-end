d1dd90b3fb08376f46edf6ea4fe441c82d051abb
alwn8918 <alwn8918@gmail.com>
2025-05-21 23:10:10 +0900

    Initial Commit

---
+++ gguro/gguroApp.swift
@@ -0,0 +1,17 @@
+//
+//  gguroApp.swift
+//  gguro
+//
+//  Created by 김미주 on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct gguroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
