40345f410a8258ae294a7969e543188681b572d6
codmsdlrltgjqm <chaeni1218@naver.com>
2025-07-15 08:05:19 +0900

    Feat: 지식대화 카테고리 선택 메뉴 UI 구현

---
+++ gguro/ViewModels/Children/LifeViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  LifeViewModel.swift
+//  gguro
+//
+//  Created by 이채은 on 7/15/25.
+//
+
+import Foundation
+
+class LifeViewModel {
+    let lifeSamples: [LifeModel] = [
+        .init(title: "손 씻기", imageName: "icon_wash"),
+        .init(title: "양치질", imageName: "icon_teeth"),
+        .init(title: "옷 입기", imageName: "icon_cloth"),
+        .init(title: "전기 조심", imageName: "icon_spark"),
+        .init(title: "마스크 쓰기", imageName: "icon_mask"),
+        .init(title: "문 닫기", imageName: "icon_door"),
+        .init(title: "손 씻기", imageName: "icon_wash"),
+        .init(title: "양치질", imageName: "icon_teeth"),
+        .init(title: "옷 입기", imageName: "icon_cloth"),
+        .init(title: "전기 조심", imageName: "icon_spark"),
+        .init(title: "마스크 쓰기", imageName: "icon_mask"),
+        .init(title: "문 닫기", imageName: "icon_door"),
+    ]
+}
