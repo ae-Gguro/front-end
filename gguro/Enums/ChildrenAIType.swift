b36dad4a1144404e465f99aa890f1a373fd9eb41
codmsdlrltgjqm <chaeni1218@naver.com>
2025-07-14 19:13:33 +0900

    Feat: 놀이 메뉴 페이지 UI 구현

--- gguro/Enums/ChildrenAIType.swift
+++ gguro/Enums/ChildrenAIType.swift
@@ -12,27 +12,37 @@ enum ChildrenAIType {
     case aiChat(name: String)
     case rolePlay(name: String)
     case wordQuiz(name: String)
+    case studyMenu(name: String)
     case lifeStudy(name: String)
     case animalStudy(name: String)
     
     var messageBox: MessageBox {
         switch self {
         case.aiChat(let name):
-            return MessageBox(content: Text("\(name)야(아)! 오늘은 어떤일들이 있었어? 👀"))
-        case.rolePlay(let name):
-            return MessageBox(content: Text("\(name)야(아)! 우리 역할 놀이 하자! 😎"))
-        case.wordQuiz(let name):
-            return MessageBox(content: Text("\(name)야(아)! 같이 단어를 맞혀 보자! 🧐"))
-        case.lifeStudy(let name):
-            return MessageBox(content: Text("\(name)야(아)! 어떤 바른 생활을 살아볼까? 🎒 "))
-        case.animalStudy(let name):
-            return MessageBox(content: Text("\(name)야(아)! 어떤 동물이 궁금해? 🐾"))
+            return MessageBox(content: Text("\(name)(아)야! 오늘은 어떤일들이 있었어? 👀"))
+        case .rolePlay(let name):
+            return MessageBox(content: Text("\(name)(아)야! 우리 역할 놀이 하자! 😎"))
+        case .wordQuiz(let name):
+            return MessageBox(content: Text("\(name)(아)야! 같이 단어를 맞혀 보자! 🧐"))
+        case .studyMenu(let name):
+            return MessageBox(content: Text("\(name)(아)야! 어떤 주제를 공부해볼까? ✏️"))
+        case .lifeStudy(let name):
+            return MessageBox(content: Text("\(name)(아)야! 어떤 바른 생활을 살아볼까? 🎒 "))
+        case .animalStudy(let name):
+            return MessageBox(content: Text("\(name)(아)야! 어떤 동물이 궁금해? 🐾"))
         }
     }
     
 }
 
 enum AIChatState {
+    case intro
+    case listening
+    case thinking
+    case ended
+}
+
+enum WordQuizState {
     case intro
     case listening
     case thinking
